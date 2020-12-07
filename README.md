a9a36f1def80be4bb2934b2ad39381a8cae030d3
zhuxiaofeng <1063446979@qq.com>
2020-12-07 16:03:25 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ElasticSearch doc and common-write（ElasticSearch doc文档摘要与工具类封装）
+
+## 什么是ElasticSearch？
+```text
+    1.ElasticSearch是一个分布式搜索与分析引擎。
+    2.Logstash与Beats工具收集聚合
+```
+
---
+++ src/main/java/com/zxf/EsConfig.java
@@ -0,0 +1,54 @@
+package com.zxf;
+
+import lombok.Data;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * ES基本配置信息类
+ * @author zxf
+ */
+@Data
+public class EsConfig {
+
+    /**
+     * Elasticsearch uri
+     */
+    private List<String> uris = Collections.singletonList("http://localhost:9200");
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 是否支持多线程
+     */
+    private boolean multiThreaded = true;
+
+    /**
+     * 链接超时时间 60s
+     */
+    private Duration connectionTimeout = Duration.ofSeconds(60L);
+
+    /**
+     * 读取时间 300s
+     */
+    private Duration readTimeout = Duration.ofSeconds(300L);
+
+    public EsConfig(String uris){
+        this.uris = Arrays.asList(uris.split(","));
+    }
+
+    public EsConfig(){
+    }
+
+}
