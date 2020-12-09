876c3957b7aa3fb78a31a1b9e673130e9014c776
zhuxiaofeng <1063446979@qq.com>
2020-12-09 21:54:19 +0800

    新增Es客户端类

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # ElasticSearch doc and common-write（ElasticSearch doc文档摘要）
 
-## 什么是ElasticSearch？
+## 一.什么是ElasticSearch？
 ```text
     1.ElasticSearch是一个分布式搜索与分析引擎。
-    2.Logstash与Beats工具收集聚合
+    2.配套工具Logstash与Beats，收集、聚合和丰富数据并存储在ElasticSearch中。(称：ELKB)
+    3.Kibana，将数据可视化，并且管理与监视堆栈。
+    4.为所有数据提供近乎实时的搜索与分析。无论结构化或非结构化文本、数值数据还是地理空间数据，Elasticsearch都可以以支持快速搜索的方式有效地存储和索引。
+    5.ElasticSearch分布式特性支持不停机部署无缝增长。
+```
+### 1.ES库中的数据(文档{es的存储方式}与索引)
+```text
+    
 ```
 
---
+++ src/main/java/com/zxf/client/JestEsClient.java
@@ -0,0 +1,80 @@
+package com.zxf.client;
+
+import com.google.gson.Gson;
+import com.zxf.EsConfig;
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.config.HttpClientConfig;
+import org.springframework.beans.factory.ObjectProvider;
+
+import java.io.IOException;
+
+/**
+ * ElasticSearch客户端链接类
+ * @author zxf
+ */
+public class JestEsClient implements EsClient{
+
+    /**
+     * 连接ElasticSearch类
+     */
+    private JestClient jestClient;
+
+    /**
+     * gson ??
+     */
+    private final Gson gson;
+
+    /**
+     * ES基本配置信息类
+     */
+    private final EsConfig esConfig;
+
+
+    public JestEsClient(ObjectProvider<Gson> gsonObjectProvider, EsConfig esConfig){
+        this.gson = gsonObjectProvider.getObject();
+        this.esConfig = esConfig;
+    }
+
+    /**
+     * 建立es连接
+     */
+    protected void buildClient(){
+        HttpClientConfig clientConfig = new HttpClientConfig.Builder(this.esConfig.getUris())
+                .discoveryEnabled(false)
+                .gson(this.gson)
+                .multiThreaded(this.esConfig.isMultiThreaded())
+                .connTimeout((int)this.esConfig.getConnectionTimeout().toMillis())
+                .readTimeout((int)this.esConfig.getReadTimeout().toMillis())
+                .build();
+
+        JestClientFactory factory = new JestClientFactory();
+        factory.setHttpClientConfig(clientConfig);
+        this.jestClient = factory.getObject();
+    }
+
+    @Override
+    public boolean existsIndex(String indexName) throws IOException {
+        return false;
+    }
+
+    @Override
+    public boolean existsIndex(String indexName, boolean includeAlias) throws IOException {
+        return false;
+    }
+
+    @Override
+    public void createIndex(String indexName) throws IOException {
+
+    }
+
+    @Override
+    public void deleteIndex(String indexName) throws IOException {
+
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
+}
