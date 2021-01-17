7fdaefd413ed6ad58c391cf492d53b8a61b56461
zhuxiaofeng <1063446979@qq.com>
2021-01-17 17:20:41 +0800

    add common

---
+++ common/src/main/java/com/zxf/BeanUtil.java
@@ -0,0 +1,49 @@
+package com.zxf;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * bean 转换工具类
+ * @author 朱晓峰
+ */
+public class BeanUtil {
+
+    /**
+     * map to bean
+     * @return T
+     */
+    public static <T> T mapToBean(Map map, Class<T> clazz){
+        JSONObject jsonObject = new JSONObject(map);
+        return jsonObject.toJavaObject(clazz);
+    }
+
+    /**
+     * maps to beans
+     * @return List<T></T>
+     */
+    public static <T> List<T> mapsToBeans(List<? extends Map> list, Class<T> clazz){
+        List<T> result = new LinkedList<>();
+        list.forEach(item -> {
+            JSONObject jsonObject = new JSONObject(item);
+            result.add(jsonObject.toJavaObject(clazz));
+        });
+        return result;
+    }
+
+    public static <T> T newInstance(Class<T> clazz){
+        try {
+            Constructor<T> constructor = clazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        }catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | InstantiationException var2){
+            throw new RuntimeException(String.format("%s , 请添加无参构造器! ", clazz.getName()));
+        }
+    }
+
+}
---
+++ common/src/test/java/com/zxf/BeanUtilTest.java
@@ -0,0 +1,59 @@
+package com.zxf;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.Data;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class BeanUtilTest {
+
+    @Test
+    public void mapToBean() {
+        Map map = new HashMap();
+        map.put("name", "xxx");
+        map.put("age", 18);
+        Persion persion = BeanUtil.mapToBean(map, Persion.class);
+        System.out.println(persion.toString());
+    }
+
+    @Test
+    public void mapsToBeans() {
+        Map map = new HashMap();
+        map.put("name", "xxx");
+        map.put("age", 18);
+        Map map2 = new HashMap();
+        map2.put("name", "xxx2");
+        map2.put("age", 18);
+        List<Map> list = new LinkedList<>();
+        list.add(map);
+        list.add(map2);
+        List<Persion> persions = BeanUtil.mapsToBeans(list, Persion.class);
+        System.out.println(JSONObject.toJSONString(persions));
+    }
+
+    @Test
+    public void newInstance() {
+        Persion persion = BeanUtil.newInstance(Persion.class);
+
+        try {
+            Student student = BeanUtil.newInstance(Student.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Data
+    public static class Persion{
+        private int age;
+        private String name;
+    }
+
+    public static class Student extends Persion{
+        public Student(String name, int age){
+        }
+    }
+}
