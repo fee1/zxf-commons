d4df386d6bd2cc5fc513006cf43430819075ba3c
zhuxiaofeng <1063446979@qq.com>
2021-08-22 16:46:28 +0800

    update

--- common/src/main/java/com/zxf/common/utils/BeanUtil.java
+++ common/src/main/java/com/zxf/common/utils/BeanUtil.java
@@ -18,7 +18,7 @@ public class BeanUtil {
      * map to bean
      * @return T
      */
-    public static <T> T mapToBean(Map map, Class<T> clazz){
+    public static <T> T mapToBean(Map<String, Object> map, Class<T> clazz){
         JSONObject jsonObject = new JSONObject(map);
         return jsonObject.toJavaObject(clazz);
     }
@@ -27,7 +27,7 @@ public class BeanUtil {
      * maps to beans
      * @return List<T></T>
      */
-    public static <T> List<T> mapsToBeans(List<? extends Map> list, Class<T> clazz){
+    public static <T> List<T> mapsToBeans(List<? extends Map<String, Object>> list, Class<T> clazz){
         List<T> result = new LinkedList<>();
         list.forEach(item -> {
             JSONObject jsonObject = new JSONObject(item);
