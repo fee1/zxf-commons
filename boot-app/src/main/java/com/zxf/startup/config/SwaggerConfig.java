4890756e210a68247657f3f05260c59f69cd6ead
zhuxiaofeng <1063446979@qq.com>
2021-09-03 20:48:00 +0800

    update swagger支持配置开启关闭

--- boot-app/src/main/java/com/zxf/startup/config/SwaggerConfig.java
+++ boot-app/src/main/java/com/zxf/startup/config/SwaggerConfig.java
@@ -3,13 +3,17 @@ package com.zxf.startup.config;
 import com.zxf.startup.config.BaseSwaggerConfig;
 import com.zxf.startup.domain.SwaggerProperties;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Swagger API文档相关配置
  * Created by macro on 2018/4/26.
  */
+@ConditionalOnProperty(value = "swagger.enable", matchIfMissing = true)
 @Configuration
+@EnableSwagger2 ////表示打开swagger功能
 public class SwaggerConfig extends BaseSwaggerConfig {
 
     @Value("${swagger.title}")
