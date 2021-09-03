e9f91ecf208a5baef891729304f177040de76cf2
zhuxiaofeng <1063446979@qq.com>
2021-09-03 19:55:24 +0800

    update swagger支持配置开启关闭

--- boot-app/src/main/java/com/zxf/startup/BootStarter.java
+++ boot-app/src/main/java/com/zxf/startup/BootStarter.java
@@ -9,7 +9,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @author 朱晓峰
  */
 @SpringBootApplication
-@EnableSwagger2 ////表示打开swagger功能
 @EnableFeignClients //表示打开Feign功能
 public class BootStarter {
 
