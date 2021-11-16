4421a12b6ce923301e819004a45420ec569ba4b4
zhuxiaofeng <1063446979@qq.com>
2021-11-16 11:05:03 +0800

    update

--- boot-app/src/main/java/com/zxf/startup/BootStarter.java
+++ boot-app/src/main/java/com/zxf/startup/BootStarter.java
@@ -3,7 +3,6 @@ package com.zxf.startup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author 朱晓峰
