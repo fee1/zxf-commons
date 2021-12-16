3eed60ca4111020ff2dd1236a6eb809bf02330a3
zhuxiaofeng <1063446979@qq.com>
2021-12-16 16:25:00 +0800

    update

--- common/src/test/java/com/zxf/BeanUtilTest.java
+++ common/src/test/java/com/zxf/BeanUtilTest.java
@@ -10,6 +10,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+@SuppressWarnings("unchecked")
 public class BeanUtilTest {
 
     @Test
--- test-common/src/main/java/com/zxf/test/BaseTest.java
+++ test-common/src/main/java/com/zxf/test/BaseTest.java
@@ -4,7 +4,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+/**
+ * 逻辑测试基类
+ *
+ * @author zxf
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = BootStarterTest.class)
-public class BaseTest {
+public abstract class BaseTest {
 }
