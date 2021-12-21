ab24281817f870f8bace5a2071541f05485b3cac
zhuxiaofeng <1063446979@qq.com>
2021-12-21 13:43:53 +0800

    update

--- auth-common/src/main/java/com/zxf/wrapper/RequestWrapper.java
+++ auth-common/src/main/java/com/zxf/wrapper/RequestWrapper.java
@@ -1,13 +1,16 @@
 package com.zxf.wrapper;
 
-import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.ReadListener;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 /**
