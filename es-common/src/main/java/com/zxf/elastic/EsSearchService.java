bbb7334e9411d10321c8d2d72d4e12aab0727ab4
zhuxiaofeng <1063446979@qq.com>
2022-04-03 20:42:09 +0800

    add note

--- es-common/src/main/java/com/zxf/elastic/EsSearchService.java
+++ es-common/src/main/java/com/zxf/elastic/EsSearchService.java
@@ -16,7 +16,6 @@ import org.springframework.util.ObjectUtils;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
