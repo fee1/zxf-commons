4e6357daa225348a5ddebaf3d6c11cab734ea67e
zhuxiaofeng <1063446979@qq.com>
2022-05-30 19:57:43 +0800

    add code file

---
+++ cache-common/src/main/java/com/zxf/cache/configuration/EnableCacheConfiguration.java
@@ -0,0 +1,20 @@
+//package com.zxf.cache.configuration;
+//
+//import org.springframework.context.annotation.Import;
+//
+//import java.lang.annotation.Documented;
+//import java.lang.annotation.ElementType;
+//import java.lang.annotation.Retention;
+//import java.lang.annotation.RetentionPolicy;
+//import java.lang.annotation.Target;
+//
+///**
+// * @author zhuxiaofeng
+// * @date 2022/5/21
+// */
+//@Target(ElementType.TYPE)
+//@Retention(RetentionPolicy.RUNTIME)
+//@Documented
+//@Import({CacheAutoConfiguration.class})
+//public @interface EnableCacheConfiguration {
+//}
