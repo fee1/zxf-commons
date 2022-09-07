95bb71991f3d4bb753aa1c0a16d1e1bde262a4c4
zhuxiaofeng <zhuxiaofeng@flashhold.com>
2022-09-07 18:31:11 +0800

    update boot-app use example

--- boot-app/src/test/java/com/kc/OtherProjectsBootStarterTest.java
+++ boot-app/src/test/java/com/kc/OtherProjectsBootStarterTest.java
@@ -1,7 +1,9 @@
 package com.kc;
 
+import com.zxf.startup.BootStarter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 /**
  * 其他项目启动类，需要加入其他项目的包名和自己公司项目的包名
@@ -9,7 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author zhuxiaofeng
  * @date 2022/9/5
  */
-@SpringBootApplication(scanBasePackages = {"com.zxf", "com.kc"})
+@SpringBootApplication
+@Import(BootStarter.class)
+//@SpringBootApplication(scanBasePackages = {"com.zxf", "com.kc"}) //这种也可以
 public class OtherProjectsBootStarterTest {
 
     public static void main(String[] args) {
