b1a635b9f62891be6db5cbe985fd44911e62ab17
zhuxiaofeng <zhuxiaofeng@flashhold.com>
2023-02-09 16:40:45 +0800

    OkHttpUtil: Add to 【Get】 request method

--- common/src/main/java/com/zxf/common/utils/OkHttpUtil.java
+++ common/src/main/java/com/zxf/common/utils/OkHttpUtil.java
@@ -10,6 +10,7 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
 import okhttp3.Headers;
+import okhttp3.HttpUrl;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -31,6 +32,11 @@ public class OkHttpUtil {
 
     private static final LazyValue<OkHttpClient> OK_HTTP_CLIENT_INST = new LazyValue<>(OkHttpClient::new);
 
+    /**
+     * 构建请求体
+     * @param body json body
+     * @return RequestBody
+     */
     private static RequestBody buildRequestBody(JSONObject body){
         return FormBody.create(
                 MediaType.parse("application/json"),
@@ -38,14 +44,87 @@ public class OkHttpUtil {
         );
     }
 
-    private static Request buildPostRequest(String url, Map<String, String> headers , JSONObject requestBody){
+    /**
+     * builder
+     * @param url 请求url
+     * @param headers 请求头
+     * @return
+     */
+    private static Request.Builder builder(String url, Map<String, String> headers){
         if (headers == null){
             headers = new HashMap<>();
         }
         return new Request.Builder()
                 .url(url)
-                .headers(Headers.of(headers))
+                .headers(Headers.of(headers));
+    }
+
+    /**
+     * builder
+     * @param url 请求url
+     * @param headers 请求头
+     * @return
+     */
+    private static Request.Builder builder(HttpUrl url, Map<String, String> headers){
+        if (headers == null){
+            headers = new HashMap<>();
+        }
+        return new Request.Builder()
+                .url(url)
+                .headers(Headers.of(headers));
+    }
+
+    /**
+     * 构建delete请求
+     * @param url 请求url
+     * @param headers 请求头
+     * @param requestBody 请求体
+     * @return
+     */
+    private static Request buildDeleteRequest(String url, Map<String, String> headers , JSONObject requestBody){
+        return builder(url, headers)
+                .delete(buildRequestBody(requestBody))
+                .build();
+    }
+
+    /**
+     * 构建put请求
+     * @param url 请求url
+     * @param headers 请求头
+     * @param requestBody 请求提
+     * @return
+     */
+    private static Request buildPutRequest(String url, Map<String, String> headers , JSONObject requestBody){
+        return builder(url, headers)
+                .put(buildRequestBody(requestBody))
+                .build();
+    }
+
+    /**
+     * 构建post请求
+     * @param url 请求url
+     * @param headers 请求头
+     * @param requestBody 请求提
+     * @return
+     */
+    private static Request buildPostRequest(String url, Map<String, String> headers , JSONObject requestBody){
+        return builder(url, headers)
                 .post(buildRequestBody(requestBody))
+                .build();
+    }
+
+    /**
+     * 构建get请求
+     * @param url 请求url
+     * @param headers 请求头
+     * @param queryParams url 参数
+     * @return
+     */
+    private static Request buildGetRequest(String url, Map<String, String> headers, Map<String,String> queryParams){
+        HttpUrl.Builder builder = HttpUrl.get(url).newBuilder();
+        queryParams.forEach(builder::addQueryParameter);
+        return builder(builder.build(), headers)
+                .get()
                 .build();
     }
 
@@ -69,12 +148,14 @@ public class OkHttpUtil {
         OK_HTTP_CLIENT_INST.get().newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-                log.error("fail request url:{}, request body:{}", call.request().url(), JSON.toJSONString(call.request().body()));
+                log.error("fail request url:{}, request body:{}", call.request().url(),
+                        JSON.toJSONString(call.request().body()));
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                log.debug("success request url:{}, response body:{}", call.request().url(), JSON.toJSONString(response.body()));
+                log.debug("success request url:{}, response body:{}", call.request().url(),
+                        JSON.toJSONString(response.body()));
             }
         });
     }
@@ -90,7 +171,8 @@ public class OkHttpUtil {
         OK_HTTP_CLIENT_INST.get().newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-                log.error("fail request url:{}, request body:{}", call.request().url(), JSON.toJSONString(call.request().body()));
+                log.error("fail request url:{}, request body:{}", call.request().url(),
+                        JSON.toJSONString(call.request().body()));
                 if (failure != null) {
                     ((FailureHandler) failure).dealWith(call, e);
                 }
@@ -98,12 +180,23 @@ public class OkHttpUtil {
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                log.debug("success request url:{}, response body:{}", call.request().url(), JSON.toJSONString(response.body()));
+                log.debug("success request url:{}, response body:{}", call.request().url(),
+                        JSON.toJSONString(response.body()));
                 if (success != null){
                     ((SuccessHandler) success).dealWith(call, response);
                 }
             }
         });
+    }
+
+    /**
+     * Response body string transform JSON
+     * @param response 响应体
+     * @return JSON
+     */
+    private static JSON toJSON(Response response) throws IOException {
+        String responseBody = response.body().string();
+        return JSON.parseObject(responseBody);
     }
 
     /**
@@ -119,8 +212,7 @@ public class OkHttpUtil {
         if (response.body() == null) {
             return new JSONObject();
         }
-        String responseBody = response.body().string();
-        return JSON.parseObject(responseBody);
+        return toJSON(response);
     }
 
     /**
@@ -134,7 +226,8 @@ public class OkHttpUtil {
      * @return tClass
      * @throws IOException
      */
-    public static <T> T post(String url, Map<String, String> headers ,Object requestBody, Class<T> tClass) throws IOException {
+    public static <T> T post(String url, Map<String, String> headers ,Object requestBody,
+                             Class<T> tClass) throws IOException {
         JSONObject responseBody = (JSONObject) post(url, headers, (JSONObject) JSON.toJSON(requestBody));
         return responseBody.toJavaObject(tClass);
     }
@@ -159,9 +252,77 @@ public class OkHttpUtil {
      * @param success 成功调用完成的后置处理
      * @param failure 失败后调用的后置处理
      */
-    public static void postAsync(String url, Map<String, String> headers, Object requestBody, Handler success, Handler failure) throws IOException {
+    public static void postAsync(String url, Map<String, String> headers, Object requestBody, Handler success,
+                                 Handler failure) throws IOException {
         Request postRequest = buildPostRequest(url, headers, (JSONObject) JSON.toJSON(requestBody));
         sendRequestAsync(postRequest, success, failure);
     }
 
+    /**
+     * 同步 get 请求
+     *
+     * @param url 请求地址
+     * @param headers 请求头
+     * @param queryParams url参数
+     * @return JSON
+     */
+    public static JSON get(String url, Map<String, String> headers, Map<String,String> queryParams) throws IOException {
+        Request getRequest = buildGetRequest(url, headers, queryParams);
+        Response response = sendRequestSync(getRequest);
+        if (response.body() == null){
+            return new JSONObject();
+        }
+        return toJSON(response);
+    }
+
+    /**
+     * 同步get请求
+     *
+     * @param url 请求地址
+     * @param headers 请求头
+     * @param queryParams url参数
+     * @param tClass 返回类型
+     * @param <T> 泛型
+     * @return
+     * @throws IOException
+     */
+    public static <T> T get(String url, Map<String, String> headers, Map<String,String> queryParams,
+                            Class<T> tClass) throws IOException {
+        JSONObject responseBody = (JSONObject) get(url, headers, queryParams);
+        return responseBody.toJavaObject(tClass);
+    }
+
+    /**
+     * 异步 get 请求
+     *
+     * @param url 请求地址
+     * @param headers 请求头
+     * @param queryParams url参数
+     * @throws IOException
+     */
+    public static void getAsync(String url, Map<String, String> headers, Map<String,String> queryParams) throws IOException {
+        Request getRequest = buildGetRequest(url, headers, queryParams);
+        sendRequestAsync(getRequest);
+    }
+
+    /**
+     * 异步 get 请求
+     *
+     * @param url 请求地址
+     * @param headers 请求头
+     * @param queryParams url参数
+     * @param success 成功调用完成的后置处理
+     * @param failure 失败后调用的后置处理
+     * @throws IOException
+     */
+    public static void getAsync(String url, Map<String, String> headers, Map<String,String> queryParams, Handler success,
+                                Handler failure) throws IOException {
+        Request getRequest = buildGetRequest(url, headers, queryParams);
+        sendRequestAsync(getRequest, success, failure);
+    }
+
+//    put
+//    delete
+//    sync
+//    async
 }
--- common/src/test/java/com/zxf/common/utils/OkHttpUtilTest.java
+++ common/src/test/java/com/zxf/common/utils/OkHttpUtilTest.java
@@ -1,5 +1,6 @@
 package com.zxf.common.utils;
 
+import com.alibaba.fastjson.JSON;
 import com.zxf.common.utils.handler.FailureHandler;
 import com.zxf.common.utils.handler.SuccessHandler;
 import com.zxf.common.utils.model.MethodExecuteMessageRequestDTO;
@@ -10,6 +11,8 @@ import okhttp3.Response;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author zhuxiaofeng
@@ -27,12 +30,14 @@ public class OkHttpUtilTest {
         methodExecuteMessageRequestDTO.setArgs(new String[]{"1", "2"});
         methodExecuteMessageRequestDTO.setReturnObj("test");
         methodExecuteMessageRequestDTO.setCostTime(1000L);
-        OkHttpUtil.postAsync("http://localhost:8080/log/server/method/execute/log/add", null, methodExecuteMessageRequestDTO);
+        Map<String, String> headers = new HashMap<>();
+        headers.put("myHeader", "test");
+        OkHttpUtil.postAsync("http://localhost:8080/log/server/method/execute/log/add", headers, methodExecuteMessageRequestDTO);
         //休眠，不然没有效果
         Thread.sleep(1000L);
 
 
-        OkHttpUtil.postAsync("http://localhost:8080/log/server/method/execute/log/add", null, methodExecuteMessageRequestDTO, (SuccessHandler) (call, response) -> {
+        OkHttpUtil.postAsync("http://localhost:8080/log/server/method/execute/log/add", headers, methodExecuteMessageRequestDTO, (SuccessHandler) (call, response) -> {
             System.out.println("成功调用了");
         }, (FailureHandler) (call, e) -> {
             System.out.println("失败调用了");
@@ -52,6 +57,37 @@ public class OkHttpUtilTest {
         methodExecuteMessageRequestDTO.setReturnObj("test");
         methodExecuteMessageRequestDTO.setCostTime(1000L);
         ResponseDTO responseDTO = OkHttpUtil.post("http://localhost:8080/log/server/method/execute/log/add", null, methodExecuteMessageRequestDTO, ResponseDTO.class);
+    }
+
+    @SneakyThrows
+    @Test
+    public void get(){
+        Map<String, String> headers = new HashMap<>();
+        headers.put("myHeader", "test");
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("name", "xiaoming");
+        queryParams.put("age", "18");
+        JSON json = OkHttpUtil.get("http://localhost:8080/get/user", headers, queryParams);
+        System.out.println(json);
+    }
+
+    @SneakyThrows
+    @Test
+    public void getAsync(){
+        Map<String, String> headers = new HashMap<>();
+        headers.put("myHeader", "test");
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("name", "xiaoming");
+        queryParams.put("age", "18");
+        OkHttpUtil.getAsync("http://localhost:8080/get/user", headers, queryParams);
+        Thread.sleep(1000L);
+
+        OkHttpUtil.getAsync("http://localhost:8080/get/user", headers, queryParams, (SuccessHandler) (call, response) -> {
+            System.out.println("成功调用了");
+        }, (FailureHandler) (call, e) -> {
+            System.out.println("失败调用了");
+        });
+        Thread.sleep(1000L);
     }
 
 }
