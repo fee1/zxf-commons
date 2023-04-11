cfca85d631f2ee4d5cdb649d7762c645a8ea8ecb
zhuxiaofeng <zhuxiaofeng@flashhold.com>
2023-04-11 16:48:53 +0800

    add: mock test

---
+++ test-common/src/main/java/com/zxf/test/MockBaseTest.java
@@ -0,0 +1,14 @@
+package com.zxf.test;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author zhuxiaofeng
+ * @date 2023/4/11
+ */
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest(classes = BootStarterTest.class)
+public class MockBaseTest {
+}
