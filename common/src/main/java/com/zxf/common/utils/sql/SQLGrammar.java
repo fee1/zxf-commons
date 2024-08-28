17edf21deb9c146451c81a916fcad6f6ffae2b0e
zhuxiaofeng <zhuxiaofeng@yilihui.com>
2024-08-28 19:08:15 +0800

    fix sql 生成工具类

--- common/src/main/java/com/zxf/common/utils/sql/SQLGrammar.java
+++ common/src/main/java/com/zxf/common/utils/sql/SQLGrammar.java
@@ -16,39 +16,39 @@ import java.util.Map;
  */
 public class SQLGrammar {
 
-    private static final String SELECT_SQL = "SELECT ";
+    private static final String SELECT = "SELECT ";
 
-    private static final String FROM_SQL = " FROM ";
+    private static final String FROM = " FROM ";
 
-    private static final String WHERE_SQL = " WHERE ";
+    private static final String WHERE = " WHERE ";
 
-    private static final String AND_SQL = " AND ";
+    private static final String AND = " AND ";
 
-    private static final String OR_SQL = " OR ";
+    private static final String OR = " OR ";
 
-    private static final String GROUP_BY_SQL = " GROUP BY ";
+    private static final String GROUP_BY = " GROUP BY ";
 
-    private static final String ORDER_BY_SQL = " ORDER BY ";
+    private static final String ORDER_BY = " ORDER BY ";
 
-    private static final String HAVING_SQL = " HAVING ";
+    private static final String HAVING = " HAVING ";
 
-    private static final String LIMIT_SQL = " LIMIT ";
+    private static final String LIMIT = " LIMIT ";
 
-    private static final String OFFSET_SQL = " OFFSET ";
+    private static final String OFFSET = " OFFSET ";
 
-    private static final String AS_SQL = " AS ";
+    private static final String AS = " AS ";
 
-    private static final String DISTINCT_SQL = " DISTINCT ";
+    private static final String DISTINCT = " DISTINCT ";
 
-    private static final String COUNT_SQL = " COUNT ";
+    private static final String COUNT = " COUNT ";
 
-    private static final String COUNT_ALL_SQL = " COUNT(*) ";
+    private static final String COUNT_ALL = " COUNT(*) ";
 
-    private static final String ASC_SQL = " ASC ";
+    private static final String ASC = " ASC ";
 
-    private static final String DESC_SQL = " DESC ";
+    private static final String DESC = " DESC ";
 
-    private static final String IN_SQL = " IN ";
+    private static final String IN = " IN ";
 
     private Class<?> entityClass;
 
@@ -100,9 +100,9 @@ public class SQLGrammar {
         String tableName = CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, simpleName);
         String sql = "";
         if (!selects.isEmpty()) {
-            sql = "select " + String.join(",", selects) + " from " + tableName;
+            sql = SELECT + String.join(",", selects) + FROM + tableName;
         }else {
-            sql = "select * from " + tableName;
+            sql = SELECT + " * " + FROM + tableName;
         }
         List<String> conditionList = new ArrayList<>();
         for (Criteria criteria : oredCriteria) {
@@ -118,10 +118,10 @@ public class SQLGrammar {
             }
         }
         if (!conditionList.isEmpty()) {
-            sql = sql + " where " + String.join(" or ", conditionList);
+            sql = sql + WHERE + String.join(OR, conditionList);
         }
         if (StringUtils.isNotEmpty(orderByClause)){
-            sql = sql + "order by " + orderByClause;
+            sql = sql + ORDER_BY + orderByClause;
         }
         return sql;
     }
