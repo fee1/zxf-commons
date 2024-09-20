9ec54a9114866f5687ea7026fa588f786649a942
zhuxiaofeng <zhuxiaofeng@yilihui.com>
2024-09-20 09:17:44 +0800

    update util

--- common/src/main/java/com/zxf/common/utils/ClassUtil.java
+++ common/src/main/java/com/zxf/common/utils/ClassUtil.java
@@ -1,5 +1,12 @@
 package com.zxf.common.utils;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
 /**
  * 类工具
  *
@@ -37,6 +44,44 @@ public class ClassUtil {
      */
     public static boolean isSubclass(Class<?> superClass, Class<?> sourceClass){
         return superClass.isAssignableFrom(sourceClass);
+    }
+
+    /**
+     * 获取包下所有类
+     * @param packageName 包名 例：cn.mezeron.jianzan.domain
+     * @return
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    public static List<Class<?>> find(String packageName) throws IOException, ClassNotFoundException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        String path = packageName.replace('.', '/');
+        Enumeration<URL> resources = classLoader.getResources(path);
+        List<Class<?>> classes = new ArrayList<>();
+
+        while (resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            File file = new File(resource.getFile());
+
+            if (file.isDirectory()) {
+                File[] files = file.listFiles();
+                for (File child : files) {
+                    if (child.isDirectory()){
+                        classes.addAll(find(packageName + "." + child.getName()));
+                    }
+                    if (child.getName().endsWith(".class")) {
+                        String className = packageName + '.' + child.getName().substring(0, child.getName().length() - 6);
+                        Class<?> aClass = Class.forName(className);
+//                        if (!aClass.isAnnotationPresent(Entity.class)){
+//                            continue;
+//                        }
+                        classes.add(aClass);
+                    }
+                }
+            }
+        }
+
+        return classes;
     }
 
 }
